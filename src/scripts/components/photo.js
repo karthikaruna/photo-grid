5309bba172ddcae98c1ad91b2de6f69941ce3848
Karthikeyan Arumugam <karthikaruna@live.com>
2020-11-29 08:00:57 +0530

    Make images responsive

--- src/scripts/components/photo.js
+++ src/scripts/components/photo.js
@@ -1,11 +1,22 @@
 export default class {
   static render(base, { photo, onZoom }) {
-    const container = document.createElement('div');
+    const container = document.createElement('div'),
+      windowWidth = window.innerWidth;
 
     container.classList.add('grid-masonry-item');
     container.insertAdjacentHTML('beforeend', `
-      <img width="${photo.width}" height="${photo.height}" 
-        src="${photo.urls.thumb}" 
+      <img width="${photo.width}" height="${photo.height}"
+        srcset="${photo.urls.thumb} 200w,
+          ${photo.urls.small} 400w,
+          ${photo.urls.regular} 1080w
+        "
+        sizes="(min-width: 1500px) ${Math.ceil(windowWidth / 6)}px,
+          (min-width: 1200px) ${Math.ceil(windowWidth / 5)}px,
+          (min-width: 992px) ${Math.ceil(windowWidth / 4)}px,
+          (min-width: 768px) ${Math.ceil(windowWidth / 3)}px,
+          (min-width: 576px) ${Math.ceil(windowWidth / 2)}px
+        "
+        src="${photo.urls.regular}"
         alt="${photo.alt_description}"
       >
       <span class="like ${localStorage.getItem(`photo:${photo.id}:liked`) ? 'liked' : ''}"></span>
