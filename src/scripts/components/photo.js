2b7ae756be6e977bd21e97a4abbedf3b1ca2df3f
Karthikeyan Arumugam <karthikaruna@live.com>
2020-11-29 08:08:43 +0530

    🧴

--- src/scripts/components/photo.js
+++ src/scripts/components/photo.js
@@ -8,14 +8,12 @@ export default class {
       <img width="${photo.width}" height="${photo.height}"
         srcset="${photo.urls.thumb} 200w,
           ${photo.urls.small} 400w,
-          ${photo.urls.regular} 1080w
-        "
+          ${photo.urls.regular} 1080w"
         sizes="(min-width: 1500px) ${Math.ceil(windowWidth / 6)}px,
           (min-width: 1200px) ${Math.ceil(windowWidth / 5)}px,
           (min-width: 992px) ${Math.ceil(windowWidth / 4)}px,
           (min-width: 768px) ${Math.ceil(windowWidth / 3)}px,
-          (min-width: 576px) ${Math.ceil(windowWidth / 2)}px
-        "
+          (min-width: 576px) ${Math.ceil(windowWidth / 2)}px"
         src="${photo.urls.regular}"
         alt="${photo.alt_description}"
       >
